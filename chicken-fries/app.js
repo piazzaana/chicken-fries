5f8532e5d071d5f020e77ab4e54feaaf5eb08a2f
Ana Piazza <piazzaana@fullsail.edu>
2018-08-07 14:41:16 -0400

    added database connection

--- chicken-fries/app.js
+++ chicken-fries/app.js
@@ -1,13 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+mongoose.connect('mongodb://localhost:27017/chicken-fries',{ useNewUrlParser:true});
 
-var app = express();
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('DATABASE CONNECTED SUCCESSFULLY');
+});
+
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
