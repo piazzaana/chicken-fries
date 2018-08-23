55549ab63df347c448c7818d5fc617374a7c04b8
Ana Piazza <piazzaana@fullsail.edu>
2018-08-23 16:12:14 -0400

    registered plugin with wordpress, no functionality yet

---
+++ wordpress/wp-content/plugins/chickenfries/chickenfries.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Chicken & Fries
+Plugin URI:  NA
+Description: Order Management Plugin
+Version:     0.0.1
+Author:      Ana Cristina Piazza
+Author URI:  https://anacristinacamargo.wordpress.org/
+Text Domain: chickenfries
+Domain Path: /languages
+License:     GPL3
+
+Chicken & Fries is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Chicken & Fries is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Chicken & Fries. If not, see https://www.gnu.org/licenses/gpl.html.
+*/
