8b8b75f9edf16e825c278fb51b90b7f57ee39c86
Ana Piazza <piazzaana@fullsail.edu>
2018-09-01 20:00:52 -0400

    started over from scratch on develop branch

--- chicken-fries/app.js
+++ chicken-fries/app.js
@@ -1,28 +1,13 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-//set up database connection
-mongoose.connect('mongodb://localhost:27017/chicken-fries',{ useNewUrlParser:true});
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-let db = mongoose.connection;
-
-//bind connection to error event
-db.on('error', console.error.bind(console, 'connection error:'));
-
-//bind connection to connection event
-db.once('open', function () {
-    console.log('DATABASE CONNECTED SUCCESSFULLY');
-});
-
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
-const app = express();
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
