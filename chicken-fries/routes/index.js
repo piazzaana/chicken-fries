2dc71a3330551ea898a368b763e8179bf22c4195
Ana Piazza <piazzaana@fullsail.edu>
2018-09-03 17:45:53 -0400

    added routes for pages on the navigation bar

--- chicken-fries/routes/index.js
+++ chicken-fries/routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+/* GET order page. */
+router.get('/order', function(req, res, next) {
+    res.render('order');
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+    res.render('about');
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+    res.render('contact');
+});
+
+/* GET location page. */
+router.get('/location', function(req, res, next) {
+    res.render('location');
 });
 
 module.exports = router;
