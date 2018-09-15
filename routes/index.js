4f2c20aa2a6d682e867b1b2c7b0ae46f0dc81d66
Ana Piazza <piazzaana@fullsail.edu>
2018-09-14 20:12:11 -0400

    added routes

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Welcome to Chicken & Fries' });
 });
 
-//get order page
-router.get('/order', function (req, res, next) {
-    res.render('order', {title:'Order Page'});
-});
-
 // get about page
 router.get('/about', function (req, res, next) {
     res.render('about', {title: 'About us page'});
@@ -24,16 +19,6 @@ router.get('/contact', function (req, res, next) {
 //get location page
 router.get('/location', function (req, res, next) {
     res.render('location',{title:'Location page'});
-});
-
-//get pick up order page
-router.get('/pickup', function (req, res, next) {
-    res.render('pickup', {title:'Pickup'});
-});
-
-//get delivery order page
-router.get('/delivery', function (req, res, next) {
-    res.render('delivery', {title:'Delivery'});
 });
 
 //get breakfast menu page
@@ -51,10 +36,6 @@ router.get('/dinner', function (req, res, next) {
     res.render('dinner', {title:'Dinner Menu'});
 });
 
-router.get('/order', function (req, res, next) {
-    res.render('order', {title: 'Order Page'});
-});
-
 router.get('/contact', function (req, res, next) {
     res.render('contact', {title: 'Contact Us'});
 });
@@ -65,10 +46,6 @@ router.get('/about', function (req, res, next) {
 
 router.get('/location', function (req, res, next) {
     res.render('location', {title: 'Our Location'});
-});
-
-router.get('/login', function (req, res, next) {
-    res.render('login', {title: 'Login Page'});
 });
 
 module.exports = router;
