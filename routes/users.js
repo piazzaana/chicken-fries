09de39e98e7fb1194e162db9f6b6c06f546e3e7d
Ana Piazza <piazzaana@fullsail.edu>
2018-09-15 22:04:07 -0400

    update users route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//get pick up order page //make it a user route
+router.get('/pickup', function (req, res, next) {
+    res.render('pickup', {title:'Pickup'});
+});
+
+//get delivery order page //make it a user route
+router.get('/delivery', function (req, res, next) {
+    res.render('delivery', {title:'Delivery'});
+});
+
+//make it a user route
+router.get('/order', function (req, res, next) {
+    res.render('order', {title: 'Order Page'});
+});
+
+//make it a user route
+router.get('/login', function (req, res, next) {
+    res.render('login', {title: 'Login Page'});
+});
+
 module.exports = router;
