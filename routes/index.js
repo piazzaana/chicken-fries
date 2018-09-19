8f2831a8b73bcd3cdf47553ba7f73d67ffe566a8
Ana Piazza <piazzaana@fullsail.edu>
2018-09-19 11:59:41 -0400

    cleaned the error on the files

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,5 @@
-<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to Chicken & Fries' });
-});
-
-//get order page
-router.get('/order', function (req, res, next) {
-    res.render('order', {title:'Order Page'});
-});
-
-// get about page
-router.get('/about', function (req, res, next) {
-    res.render('about', {title: 'About us page'});
-});
-
-//get contact page
-router.get('/contact', function (req, res, next) {
-    res.render('contact', {title:'Contact Us page'});
-=======
-const express = require('express');
-const router = express.Router();
 const Breakfast = require('../models/breakfast');
 const Lunch = require('../models/lunch');
 const Dinner = require('../models/dinner');
@@ -35,43 +12,6 @@ router.get('/', function(req, res, next) {
 // get about page
 router.get('/about', function (req, res, next) {
     res.render('about', {title: 'About us page'});
-});
-
-//get contact page
-router.get('/contact', function (req, res, next) {
-    res.render('contact');
->>>>>>> 5f01b950e8dc359311d8dc637cb5e46f38e787ce
-});
-
-//get location page
-router.get('/location', function (req, res, next) {
-    res.render('location',{title:'Location page'});
-<<<<<<< HEAD
-});
-
-//get pick up order page
-router.get('/create-pick-up-order', function (req, res, next) {
-    res.render('create-pick-up-order', {title:'Create an order for pick up'});
-});
-
-//get delivery order page
-router.get('/create-delivery-order', function (req, res, next) {
-    res.render('create-delivery-order', {title:'Create an order for delivery'});
-});
-
-//get breakfast menu page
-router.get('/breakfast', function (req, res, next) {
-    res.render('breakfast', {title: 'Breakfast Menu'});
-});
-
-//get lunch page
-router.get('/lunch', function (req, res, next) {
-    res.render('lunch', {title:'Lunch Menu'});
-});
-
-//get dinner menu
-router.get('/dinner', function (req, res, next) {
-    res.render('dinner', {title:'Dinner Menu'});
 });
 
 router.get('/order', function (req, res, next) {
@@ -92,7 +32,7 @@ router.get('/location', function (req, res, next) {
 
 router.get('/login', function (req, res, next) {
     res.render('login', {title: 'Login Page'});
-=======
+
 });
 
 //get breakfast menu page
@@ -114,7 +54,6 @@ router.get('/dinner', function (req, res, next) {
     Dinner.find(function (err, docs) {
         res.render('dinner', {title:'Dinner Menu', dinner:docs});
     });
->>>>>>> 5f01b950e8dc359311d8dc637cb5e46f38e787ce
 });
 
 module.exports = router;
