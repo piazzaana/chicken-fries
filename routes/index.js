3d9022142cd8965edeafbb876634366eb3002960
Ana Piazza <piazzaana@fullsail.edu>
2018-09-19 16:14:35 -0400

    displaying items in the cart

--- routes/index.js
+++ routes/index.js
@@ -93,7 +93,11 @@ router.get('/add-to-cart/dinner/:id', function (req, res, next) {
 });
 
 router.get('/shopping-cart', function (req, res, next) {
-    res.render('shopping-cart');
+    if(!req.session.cart){
+        return res.render('shopping-cart',{foodItems: null});
+    }
+    let cart = new Cart(req.session.cart);
+    res.render('shopping-cart', {foodItems: cart.generateArray(), totalPrice: cart.totalPrice})
 });
 
 module.exports = router;
