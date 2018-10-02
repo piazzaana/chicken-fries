8514d893861d67ce3b57d71198cbc9bb2aea7f6c
Ana Piazza <piazzaana@fullsail.edu>
2018-10-02 10:23:57 -0400

    removed unnecessary routes, updated syntax to es6 on index.js file

--- routes/index.js
+++ routes/index.js
@@ -8,48 +8,31 @@ const Cart = require('../models/cart');
 const Order = require('../models/order');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
     res.render('index', { title: 'Welcome to Chicken & Fries' });
 });
 
-router.get('/order', function (req, res, next) {
-    res.render('order', {title: 'Order Page'});
-});
-
 // get about page
-router.get('/about', function (req, res, next) {
+router.get('/about', (req, res, next) => {
     res.render('about', {title: 'About us page'});
 });
 
-router.get('/order', function (req, res, next) {
-    res.render('order', {title: 'Order Page'});
-});
-
-router.get('/contact', function (req, res, next) {
+router.get('/contact', (req, res, next) => {
     res.render('contact', {title: 'Contact Us'});
 });
 
-router.get('/about', function (req, res, next) {
-    res.render('about', {title: 'About Us'});
-});
-
-router.get('/location', function (req, res, next) {
+router.get('/location', (req, res, next) => {
     res.render('location', {title: 'Our Location'});
 });
 
-router.get('/login', function (req, res, next) {
-    res.render('login', {title: 'Login Page'});
-
-});
-
 //get breakfast menu page
-router.get('/breakfast', function (req, res, next) {
+router.get('/breakfast', (req, res, next) => {
     Breakfast.find(function (err, docs) {
         res.render('menus/breakfast', {title: 'Breakfast Menu', breakfast: docs});
     });
 });
 
-router.get('/add-to-cart/breakfast/:id', function (req, res, next) {
+router.get('/add-to-cart/breakfast/:id', (req, res, next) => {
     let bfItemId = req.params.id;
     let cart = new Cart(req.session.cart ? req.session.cart : {});
     Breakfast.findById(bfItemId, function (err, bfItem) {
@@ -64,13 +47,13 @@ router.get('/add-to-cart/breakfast/:id', function (req, res, next) {
 });
 
 //get lunch page
-router.get('/lunch', function (req, res, next) {
+router.get('/lunch', (req, res, next) => {
     Lunch.find(function (err, docs) {
         res.render('menus/lunch', {title:'Lunch Menu', lunch: docs});
     });
 });
 
-router.get('/add-to-cart/lunch/:id', function (req, res, next) {
+router.get('/add-to-cart/lunch/:id', (req, res, next) => {
     let lunchItemId = req.params.id;
     let cart = new Cart(req.session.cart ? req.session.cart : {});
     Lunch.findById(lunchItemId, function (err, lunchItem) {
@@ -85,13 +68,13 @@ router.get('/add-to-cart/lunch/:id', function (req, res, next) {
 });
 
 //get dinner menu
-router.get('/dinner', function (req, res, next) {
+router.get('/dinner', (req, res, next) => {
     Dinner.find(function (err, docs) {
         res.render('menus/dinner', {title:'Dinner Menu', dinner:docs});
     });
 });
 
-router.get('/add-to-cart/dinner/:id', function (req, res, next) {
+router.get('/add-to-cart/dinner/:id', (req, res, next) => {
     let dinnerItemId = req.params.id;
     let cart = new Cart(req.session.cart ? req.session.cart : {});
     Dinner.findById(dinnerItemId, function (err, dinnerItem) {
@@ -105,7 +88,7 @@ router.get('/add-to-cart/dinner/:id', function (req, res, next) {
     });
 });
 
-router.get('/shopping-cart', function (req, res, next) {
+router.get('/shopping-cart', (req, res, next) => {
     if(!req.session.cart){
         return res.render('shopping-cart',{foodItems: null});
     }
@@ -113,7 +96,7 @@ router.get('/shopping-cart', function (req, res, next) {
     res.render('shopping-cart', {foodItems: cart.generateArray(), totalPrice: cart.totalPrice})
 });
 
-router.get('/checkout', isLoggedIn, function (req, res, next) {
+router.get('/checkout', isLoggedIn, (req, res, next) => {
     if(!req.session.cart){
         return res.redirect('/shopping-cart');
     }
@@ -121,12 +104,12 @@ router.get('/checkout', isLoggedIn, function (req, res, next) {
     res.render('checkout',{total: cart.totalPrice});
 });
 
-router.post('/checkout', isLoggedIn, function (req, res, next) {
+router.post('/checkout', isLoggedIn, (req, res, next) => {
     if(!req.session.cart){
         return res.redirect('/shopping-cart');
     }
     let cart = new Cart(req.session.cart);
-    let stripe = require("stripe")("sk_test_Nm4ul2p0g79wCgLhUB5xBZoD");
+    let stripe = require("stripe")(process.env.SECRET_KEY);
 
     stripe.charges.create({
         amount: cart.totalPrice * 100,
